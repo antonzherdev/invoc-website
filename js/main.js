61baf461f0d62d9c43e0f4b9e87702bdab96b85b
Anton Zherdev <anton.zherdev@icloud.com>
2020-06-19 20:19:25 +1200

    ajax registration

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 document.getElementById("timezone").value = Intl.DateTimeFormat().resolvedOptions().timeZone
-document.getElementById("nav-sign-up").href = `https://s.${location.host}/sing-up`
+document.getElementById("nav-sign-up").href = `https://s.${location.host}/sign-up`
 document.getElementById("nav-login").href = `https://s.${location.host}/`
-document.getElementById("sign-up-form").action = `https://private.${location.host}/sign-up`
 
+$("#sign-up-form").submit(function(e) {
+  e.preventDefault();
+  const form = $(this);
+  form.parent().addClass("m-signing-up")
+  const url = `https://private.invoc.xyz/sign-up`;
+  $("#reg-email").text($("#email").val());
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: form.serialize(),
+    success: function()
+    {
+      form.parent().removeClass("m-signing-up")
+      form.parent().addClass("m-signed-up")
+    }
+  });
+});
