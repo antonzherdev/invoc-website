fe1302ef323100c10c42638b9291a0b869e978d5
Anton Zherdev <anton.zherdev@icloud.com>
2020-06-24 21:58:42 +1200

    improve

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-document.getElementById("timezone").value = Intl.DateTimeFormat().resolvedOptions().timeZone
+$(".timezone").val(Intl.DateTimeFormat().resolvedOptions().timeZone);
 document.getElementById("nav-sign-up").href = `https://s.${location.host}/sign-up`
 document.getElementById("nav-login").href = `https://s.${location.host}/`
 
-$("#sign-up-form").submit(function(e) {
+$(".sign-up-form > form").submit(function(e) {
   e.preventDefault();
   const form = $(this);
   form.parent().addClass("m-signing-up")
